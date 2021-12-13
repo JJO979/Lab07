95be5b6ba42b92d05b3180139920805a1d9d1e3f
Jeremy Joseph <jjoseph4@wisc.edu>
2021-12-13 16:53:43 -0600

    complete

--- mapper.py
+++ mapper.py
@@ -2,12 +2,18 @@
 
 import sys
 
+
 def mapper():
-    for line in sys.stdin:
-        data = line.strip().split(",")
-        timestamp, city, item, cost = data
-        print(city + "," + cost)
+	for line in sys.stdin:
+		try:
+			data = line.strip().split(",")
+			timestamp, city, item, cost = data
+			print(city + "," + cost)
+
+		except:
+			continue
+
 
 if __name__ == "__main__":
-    # what function should run when python mapper.py is called?
-    mapper()
+	# what function should run when python mapper.py is called?
+	mapper()
